ce175c3940473c4147f6e16a6e3ef50ebb264de6
ak <akarashov@gmail.com>
2025-10-15 20:53:30 +0300

    fix flag iter4

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,7 +8,7 @@ type Config struct {
 }
 
 func (c *Config) New() {
-	flag.StringVar(&c.Listen, "a", "127.0.0.1:8888", "Listen on")
-	flag.StringVar(&c.Base, "b", "http://127.0.0.1:8000/", "Base address")
+	flag.StringVar(&c.Listen, "a", "127.0.0.1:8080", "Listen on")
+	flag.StringVar(&c.Base, "b", "http://127.0.0.1:8080/", "Base address")
 	flag.Parse()
 }
