96e1a0012dbc7e6551ad4dabe9c38e048beb8416
ak <akarashov@gmail.com>
2025-10-25 23:38:09 +0300

    iter6

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"sync"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/akarashov/urltamer/internal/config"
+	"go.uber.org/zap"
 )
 
 var (
@@ -19,9 +21,21 @@ var (
 
 const tamerLength = 8
 
-type Handler struct {
+type (
+	Handler struct {
 	Base *string
-}
+	}
+
+    responseData struct {
+        status int
+        size int
+    }
+
+    loggingResponseWriter struct {
+        http.ResponseWriter
+        responseData *responseData
+    }
+)
 
 func makeTamer() string {
 	buffer := make([]byte, tamerLength)
@@ -71,5 +85,47 @@ func (h *Handler) ResponseEndpoint(res http.ResponseWriter, req *http.Request) {
 		http.Redirect(res, req, reqURL, http.StatusTemporaryRedirect)
 	} else {
 		http.Error(res, "Not found", http.StatusBadRequest)
+	}
+}
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+    size, err := r.ResponseWriter.Write(b) 
+    r.responseData.size += size
+    return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+    r.ResponseWriter.WriteHeader(statusCode) 
+    r.responseData.status = statusCode
+}
+
+func LoggingMiddlewareRequest(wrapped http.HandlerFunc, sl zap.SugaredLogger) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		start := time.Now()
+		wrapped(res, req)
+		duration := time.Since(start)
+		sl.Infoln(
+            "uri", req.RequestURI,
+            "method", req.Method,
+            "duration", duration,
+        )
+	}
+}
+
+func LoggingMiddlewareResponse(wrapped http.HandlerFunc, sl zap.SugaredLogger) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		responseData := &responseData {
+			status: 0,
+            size: 0,
+        }
+        lres := loggingResponseWriter {
+        	ResponseWriter: res,
+            responseData: responseData,
+        }
+		wrapped(&lres, req)
+		sl.Infoln(
+            "status", responseData.status,
+            "size", responseData.size,
+        )
 	}
 }
