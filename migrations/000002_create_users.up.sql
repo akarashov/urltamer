ac307c4f10e14f6194796768e70c2e72e82b3684
akarashov <akarashov@gmail.com>
2025-11-27 22:50:16 +0300

    iter14 h

--- migrations/000002_create_users.up.sql
+++ migrations/000002_create_users.up.sql
@@ -4,6 +4,6 @@ CREATE TABLE users (
 INSERT INTO users (id) VALUES (1);
 
 ALTER TABLE tamers ADD COLUMN user_id INT NOT NULL DEFAULT 1;
-ALTER TABLE tamers ADD CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id);
+-- ALTER TABLE tamers ADD CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id);
 ALTER TABLE tamers ADD CONSTRAINT unique_user_original_url UNIQUE(user_id, original_url);
 
