d118d7a928ed6e5869fece9653f34bd916cd39b4
akarashov <akarashov@gmail.com>
2026-02-12 20:57:45 +0300

    iter23 a

--- internal/utils/generator.go
+++ internal/utils/generator.go
@@ -19,3 +19,13 @@ func GenerateShortURL() string {
 	tamer := base64.RawURLEncoding.EncodeToString(buffer)
 	return tamer[:tamerLength]
 }
+
+// NA returns "N/A" if the input string is empty, otherwise it returns the input string.
+func NA(s string) string {
+	switch s {
+	case "":
+		return "N/A"
+	default:
+		return s
+	}
+}
